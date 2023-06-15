552cf871b8ac9b98e287e7c9573f4f51476ab509
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2023-06-15 09:53:01 +0100

    Updated code to run on latest Django

--- migrator/migrator/management/commands/migratortester.py
+++ migrator/migrator/management/commands/migratortester.py
@@ -1,35 +1,35 @@
 from django.core.management.base import BaseCommand, CommandError
-from migrator import models
-import _populate as populate
-import _test as test
+from . import _populate as populate
+from . import _test as test
+
 
 class Command(BaseCommand):
-    help = 'Manage migrator operations'
+    help = "Manage migrator operations"
 
     def add_arguments(self, parser):
-        parser.add_argument('operation')
+        parser.add_argument("operation")
 
     def handle(self, *args, **options):
         try:
-            operation = options['operation']
+            operation = options["operation"]
         except KeyError:
             operation = args[0]
 
-        if operation == 'start':
-            self.stdout.write('Create legions')
+        if operation == "start":
+            self.stdout.write("Create legions")
             populate.create_legions(self)
-            self.stdout.write('Create projects and workgroups')
+            self.stdout.write("Create projects and workgroups")
             populate.create_projects_and_workgroups(self)
-            self.stdout.write('Create organizations')
+            self.stdout.write("Create organizations")
             populate.create_organizations(self)
-            self.stdout.write('Create organizations (subclasses)')
+            self.stdout.write("Create organizations (subclasses)")
             populate.create_organizations_subclasses(self)
 
-        self.stdout.write('Test legions')
+        self.stdout.write("Test legions")
         test.test_legions(self)
-        self.stdout.write('Test projects and workgroups')
+        self.stdout.write("Test projects and workgroups")
         test.test_projects_and_workgroups(self)
-        self.stdout.write('Test organizations')
+        self.stdout.write("Test organizations")
         test.test_organizations(self)
-        self.stdout.write('Test organizations (subclasses)')
+        self.stdout.write("Test organizations (subclasses)")
         test.test_organizations_subclasses(self)
--- migrator/migrator/settings.py
+++ migrator/migrator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ewaw&q$3wi%w#1z^)x$&5nz#j66ufj8-dx4qad1#_^as_p2vgs'
+SECRET_KEY = "ewaw&q$3wi%w#1z^)x$&5nz#j66ufj8-dx4qad1#_^as_p2vgs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,63 +31,63 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'django_extensions',
-    'guardian',
-
-    'groups_manager',
-    'migrator',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "guardian",
+    "groups_manager",
+    "migrator",
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'guardian.backends.ObjectPermissionBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "guardian.backends.ObjectPermissionBackend",
 )
 ANONYMOUS_USER_ID = -1
 
-ROOT_URLCONF = 'migrator.urls'
+ROOT_URLCONF = "migrator.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'migrator.wsgi.application'
+WSGI_APPLICATION = "migrator.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -95,9 +95,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -109,20 +109,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 GROUPS_MANAGER = {
-    'AUTH_MODELS_SYNC': True,
-    'AUTH_MODELS_GET_OR_CREATE': False,
-    'GROUP_NAME_PREFIX': '',
-    'GROUP_NAME_SUFFIX': '',
-    'USER_USERNAME_PREFIX': '',
-    'USER_USERNAME_SUFFIX': '',
-    'PERMISSIONS': {
-        'owner': ['view', 'change', 'delete'],
-        'group': ['view', 'change'],
-        'groups_upstream': ['view'],
-        'groups_downstream': [],
-        'groups_siblings': ['view'],
+    "AUTH_MODELS_SYNC": True,
+    "AUTH_MODELS_GET_OR_CREATE": False,
+    "GROUP_NAME_PREFIX": "",
+    "GROUP_NAME_SUFFIX": "",
+    "USER_USERNAME_PREFIX": "",
+    "USER_USERNAME_SUFFIX": "",
+    "PERMISSIONS": {
+        "owner": ["view", "change", "delete"],
+        "group": ["view", "change"],
+        "groups_upstream": ["view"],
+        "groups_downstream": [],
+        "groups_siblings": ["view"],
     },
 }
